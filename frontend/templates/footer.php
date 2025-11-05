2eda9f6576634ba13dc75c04da5558eb0925161f
Figlio Otniel <otnielfiglio@gmail.com>
2025-11-05 15:56:20 +0700

    Final cleanup: Removed all unnecessary root files and folders

--- frontend/templates/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-            <footer class="content-footer">
-                <p>&copy; <?php echo date('Y'); ?> Pemerintah Desa Makmur Jaya. Diberdayakan oleh SITANAS.</p>
-            </footer>
-        </div>
-    </div>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
